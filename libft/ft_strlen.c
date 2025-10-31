8dda05f808ce65c7e7553ecc3a2b454b0e1afb75
nourrminaa <184600463+nourrminaa@users.noreply.github.com>
2025-10-31 14:31:33 +0200

    ft_strlen + testing main

---
+++ libft/ft_strlen.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmina <nmina@student.42beirut.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/31 14:24:21 by nmina             #+#    #+#             */
+/*   Updated: 2025/10/31 14:30:59 by nmina            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include<unistd.h>
+// #include<stdio.h>
+int	ft_strlen(const char *s)
+{
+	int	length;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+// int main(void)
+// {
+//     const char str[] = "hi! it's nour";
+//     int length;
+
+//     length = ft_strlen(str);
+//     printf("Length: %d\n", length);
+//     return 0;
+// }
