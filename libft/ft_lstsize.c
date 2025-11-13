66e9a79c307a0514191a0d589e05ed69d7330e44
nourrminaa <184600463+nourrminaa@users.noreply.github.com>
2025-11-14 01:41:04 +0200

    fct: ft_lstsize.c

---
+++ libft/ft_lstsize.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmina <nmina@student.42beirut.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/14 01:38:25 by nmina             #+#    #+#             */
+/*   Updated: 2025/11/14 01:40:17 by nmina            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int		count;
+	t_list	*current;
+
+	count = 0;
+	current = lst;
+	while (current)
+	{
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
