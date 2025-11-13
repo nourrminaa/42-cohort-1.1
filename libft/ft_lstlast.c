1435024038e856cba10591e2e1190e19226e2626
nourrminaa <184600463+nourrminaa@users.noreply.github.com>
2025-11-14 01:43:45 +0200

    fct: ft_lstlast.c

---
+++ libft/ft_lstlast.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmina <nmina@student.42beirut.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/14 01:41:46 by nmina             #+#    #+#             */
+/*   Updated: 2025/11/14 01:42:54 by nmina            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*current;
+
+	if (!lst)
+		return (NULL);
+	current = lst;
+	while (current->next)
+		current = current->next;
+	return (current);
+}
