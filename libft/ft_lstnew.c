a637beb896ccfd1c18bb3c0f895d0fac30ab79ac
nourrminaa <184600463+nourrminaa@users.noreply.github.com>
2025-11-14 01:35:25 +0200

    fct: ft_lstnew.c

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmina <nmina@student.42beirut.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/14 01:33:22 by nmina             #+#    #+#             */
+/*   Updated: 2025/11/14 01:34:45 by nmina            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
+}
