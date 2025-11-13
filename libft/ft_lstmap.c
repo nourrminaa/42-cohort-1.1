5b31a43f3afd138bf27be2de62e9abfc536a2844
nourrminaa <184600463+nourrminaa@users.noreply.github.com>
2025-11-14 01:57:35 +0200

    fct: ft_lstmap.c

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmina <nmina@student.42beirut.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/14 01:55:48 by nmina             #+#    #+#             */
+/*   Updated: 2025/11/14 01:57:12 by nmina            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_list;
+	t_list	*new_node;
+	t_list	*current;
+
+	if (!lst || !f)
+		return (NULL);
+	new_list = NULL;
+	current = lst;
+	while (current)
+	{
+		new_node = ft_lstnew(f(current->content));
+		if (!new_node)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_node);
+		current = current->next;
+	}
+	return (new_list);
+}
