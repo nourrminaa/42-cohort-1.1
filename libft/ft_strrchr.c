a4e7e5573260a9fb205b5245baa3f3e68ee4636f
nourrminaa <184600463+nourrminaa@users.noreply.github.com>
2025-11-14 02:29:01 +0200

    fix: wrong ch decleration

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: nmina <nmina@student.42beirut.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 17:31:38 by nmina             #+#    #+#             */
-/*   Updated: 2025/11/13 21:26:53 by nmina            ###   ########.fr       */
+/*   Updated: 2025/11/14 02:07:41 by nmina            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	int		i;
-	char	*ch;
+	char	ch;
 
 	if (!s)
 		return (NULL);
 	i = ft_strlen(s);
-	ch = (char *)c;
+	ch = (char)c;
 	while (i >= 0)
 	{
-		if (s[i] == (char)ch)
-			return (&s[i]);
+		if (s[i] == ch)
+			return ((char *)&s[i]);
 		i--;
 	}
 	return (NULL);
