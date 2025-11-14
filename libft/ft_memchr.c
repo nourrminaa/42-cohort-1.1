5c7ae7a79a35afd59da90ef2e720a9cb5ae2a247
nourrminaa <184600463+nourrminaa@users.noreply.github.com>
2025-11-14 03:21:11 +0200

    fix: method name in ft_memchr.c

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,13 +6,13 @@
 /*   By: nmina <nmina@student.42beirut.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 21:28:08 by nmina             #+#    #+#             */
-/*   Updated: 2025/11/13 21:32:06 by nmina            ###   ########.fr       */
+/*   Updated: 2025/11/14 03:20:57 by nmina            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t			i;
 	unsigned char	*str;
